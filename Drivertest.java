5be9702feea5c2d4ed72f61d90612087ce78908d
Dbauer1010 <56281163+Dbauer1010@users.noreply.github.com>
2019-10-07 19:41:02 -0500

    Add files via upload

---
+++ Drivertest.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		String s = "David";
+		char c = 'D';
+		
+		System.out.println(s.toUpperCase());
+		System.out.println(Character.toUpperCase(c));
+		System.out.println(s.toUpperCase());
+	}
+		
+}
